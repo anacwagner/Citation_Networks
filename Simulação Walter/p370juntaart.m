d48305eb86d86b77d32af89fb82efe9abb9ac15b
Ana Carolina Wagner <acwgdb@gmail.com>
2019-03-12 17:34:56 -0300

    Update p370juntaart.m

--- Simulação Walter/p370juntaart.m
+++ Simulação Walter/p370juntaart.m
@@ -1,44 +1,44 @@
 function [a,cit]=p370juntaart(a,an,cit,citn)
-%anexa as listas de artigos e citações gerados no período
-%às listas de artigos e citações acumuladas
+%anexa as listas de artigos e citaÃ§Ãµes gerados no perÃ­odo
+%Ã s listas de artigos e citaÃ§Ãµes acumuladas
 %atualiza a coluna 5 com o valor de fitness acumulado
 
 %
-%TEM QUE ACERTAR PARA RECEBER OS ARTIGOS NORMAIS E OS ESTRATÉGICOS
+%TEM QUE ACERTAR PARA RECEBER OS ARTIGOS NORMAIS E OS ESTRATÃ‰GICOS
 %
 
-%parâmetros de entrada:
-    %a: lista de artigos existentes até o período passado
-    %an: lista de artigos publicados neste período
-    %cit: lista de citações existentes
-    %citn: lista de citações dos artigos deste período
+%parÃ¢metros de entrada:
+    %a: lista de artigos existentes atÃ© o perÃ­odo passado
+    %an: lista de artigos publicados neste perÃ­odo
+    %cit: lista de citaÃ§Ãµes existentes
+    %citn: lista de citaÃ§Ãµes dos artigos deste perÃ­odo
     
-%SAÍDA/ENTRADA
+%SAÃDA/ENTRADA
 %formato de "cit" e "citn"
-    %coluna 01: código do artigo SOURCE
+    %coluna 01: cÃ³digo do artigo SOURCE
     %coluna 02: ano do artigo SOURCE
-    %coluna 03: periódico do artigo SOURCE
-    %coluna 04: código do artigo TARGET
+    %coluna 03: periÃ³dico do artigo SOURCE
+    %coluna 04: cÃ³digo do artigo TARGET
     %coluna 05: ano do artigo TARGET
-    %coluna 06: periódico do artigo TARGET
-    %coluna 07: edição do periódico do artigo SOURCE 
-    %coluna 08: edição do periódico do artigo TARGET
+    %coluna 06: periÃ³dico do artigo TARGET
+    %coluna 07: mÃªs do artigo SOURCE (ediÃ§Ã£o do periÃ³dico)
+    %coluna 08: mÃªs do artigo TARGET (ediÃ§Ã£o do periÃ³dico)
 
 %formato de "a" e "an"
-    %coluna 01: código do periódico
-    %coluna 02: código do artigo
+    %coluna 01: cÃ³digo do periÃ³dico
+    %coluna 02: cÃ³digo do artigo
     %coluna 03: ano do artigo
-    %coluna 04: fitness do artigo (lognormal com parâmetros de média e desvio do periódico)
+    %coluna 04: fitness do artigo (lognormal com parÃ¢metros de mÃ©dia e desvio do periÃ³dico)
     %coluna 05: fitness acumulado
-    %coluna 06: quantidade de citações do artigo (poisson média ncit)
-    %coluna 07: grau de entrada do artigo - total de citações recebidas
+    %coluna 06: quantidade de citaÃ§Ãµes do artigo (poisson mÃ©dia ncit)
+    %coluna 07: grau de entrada do artigo - total de citaÃ§Ãµes recebidas
     %coluna 08: fitness ponderado por grau (fit*grau)
     %coluna 09: fitness ponderado acumulado
-    %coluna 10: edição do periódico em que o artigo foi publicado
+    %coluna 10: mÃªs em que o artigo foi publicado (controle da ediÃ§Ã£o)
     
 a=[a;an];
 afim=[a(:,6:8)];
-aed=[a(:,10)]; % salva a coluna com a edição dos periódicos
+aed=[a(:,10)]; % salva a coluna com a ediÃ§Ã£o dos periÃ³dicos
 %substitui a coluna de valores acumulados de fitness (col 5) com os valores atuais
 a=[a(:,1:4) cumsum(a(:,4)) afim];
 a=[a cumsum(a(:,8)) aed];
