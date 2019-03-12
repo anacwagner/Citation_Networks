898f54a3945fe9a6dbfe6f7af4b82ddc5cb884dc
Ana Carolina Wagner <acwgdb@gmail.com>
2019-03-12 17:26:53 -0300

    Update p361geracitano_nNEW.m

--- Simulação Walter/p361geracitano_nNEW.m
+++ Simulação Walter/p361geracitano_nNEW.m
@@ -1,219 +1,96 @@
 function cit=p361geracitano_nNEW(a,an)
-%gera o arquivo "cit" com as citações dos artigos de um ano 
-%PROCESSO NORMAL: base completa dos artigos, citações dos artigos "normais"
+%gera o arquivo "cit" com as citaÃ§Ãµes dos artigos de um ano 
+%PROCESSO NORMAL: base completa dos artigos, citaÃ§Ãµes dos artigos "normais"
 %exatamente igual ao p060
-%replicado por finalidade de organização: separação do processo estratégico
-%tem que rodar com an = artigos "normais" gerados no período
+%replicado por finalidade de organizaÃ§Ã£o: separaÃ§Ã£o do processo estratÃ©gico
+%tem que rodar com an = artigos "normais" gerados no perÃ­odo
 %
-%parâmetros de entrada:
-    %a: lista de artigos existentes até o período passado
-    %an: lista de artigos publicados neste período - LISTA DE ARTIGOS "NORMAIS"
+%parÃ¢metros de entrada:
+    %a: lista de artigos existentes atÃ© o perÃ­odo passado
+    %an: lista de artigos publicados neste perÃ­odo - LISTA DE ARTIGOS "NORMAIS"
 
-%considera grau de entrada e valor de fitness de cada artigo para seleção
-%estratégia de seleção: 
+%considera grau de entrada e valor de fitness de cada artigo para seleÃ§Ã£o
+%estratÃ©gia de seleÃ§Ã£o: 
     %50% apenas pelo fitness
     %50% pelo fitness ponderado pelo grau de entrada
 
-%SAÍDA
+%SAÃDA
 %formato de "cit"
-    %coluna 01: código do artigo SOURCE
+    %coluna 01: cÃ³digo do artigo SOURCE
     %coluna 02: ano do artigo SOURCE
-    %coluna 03: periódico do artigo SOURCE
-    %coluna 04: código do artigo TARGET
+    %coluna 03: periÃ³dico do artigo SOURCE
+    %coluna 04: cÃ³digo do artigo TARGET
     %coluna 05: ano do artigo TARGET
-    %coluna 06: periódico do artigo TARGET
-    %coluna 07: edição do periódico do artigo SOURCE 
-    %coluna 08: edição do periódico do artigo TARGET
+    %coluna 06: periÃ³dico do artigo TARGET
+    %coluna 07: mÃªs do artigo SOURCE (ediÃ§Ã£o do periÃ³dico)
+    %coluna 08: mÃªs do artigo TARGET (ediÃ§Ã£o do periÃ³dico)
 
 %ENTRADA
 %formato de "a" e "an"
-    %coluna 01: código do periódico
-    %coluna 02: código do artigo
+    %coluna 01: cÃ³digo do periÃ³dico
+    %coluna 02: cÃ³digo do artigo
     %coluna 03: ano do artigo
-    %coluna 04: fitness do artigo (lognormal com parâmetros de média e desvio do periódico)
+    %coluna 04: fitness do artigo (lognormal com parÃ¢metros de mÃ©dia e desvio do periÃ³dico)
     %coluna 05: fitness acumulado
-    %coluna 06: quantidade de citações do artigo (poisson média ncit)
-    %coluna 07: grau de entrada do artigo - total de citações recebidas
+    %coluna 06: quantidade de citaÃ§Ãµes do artigo (poisson mÃ©dia ncit)
+    %coluna 07: grau de entrada do artigo - total de citaÃ§Ãµes recebidas
     %coluna 08: fitness ponderado por grau (fit*grau)
     %coluna 09: fitness ponderado acumulado
-    %coluna 10: edição do periódico em que o artigo foi publicado
+    %coluna 10: mÃªs em que o artigo foi publicado (controle da ediÃ§Ã£o)
 
-cit=zeros(sum(an(:,6)),8); %inicializa o arquivo com as citações
-                           %sum(an(:,6)): total de citações dos artigos do ano
-seq=0; %variável auxiliar para controlar a sequência de citações
-for art=1:size(an,1)      %para cada artigo de an que servirá de SOURCE
-    c=an(art,6);          %quantidade de citações do artigo
-    %variáveis auxiliares para contagem de citações por tipo de estratégia
+cit=zeros(sum(an(:,6)),8); %inicializa o arquivo com as citaÃ§Ãµes
+                           %sum(an(:,6)): total de citaÃ§Ãµes dos artigos do ano
+seq=0; %variÃ¡vel auxiliar para controlar a sequÃªncia de citaÃ§Ãµes
+for art=1:size(an,1)      %para cada artigo de an que servirÃ¡ de SOURCE
+    c=an(art,6);          %quantidade de citaÃ§Ãµes do artigo
+    %variÃ¡veis auxiliares para contagem de citaÃ§Ãµes por tipo de estratÃ©gia
     %tipo1 - fitness ponderado por grau
     %tipo2 - apenas fitness
-    %conta quantas citaçoes do tipo 1
+    %conta quantas citaÃ§oes do tipo 1
     tipo1=0;
     for sel=1:c
-        if rand(1)<0.5     %probabilidade de 50% de citação baseada em fitness ponderado
+        if rand(1)<0.5     %probabilidade de 50% de citaÃ§Ã£o baseada em fitness ponderado
             tipo1=tipo1+1;
         end
     end
     tipo2=c-tipo1;
     indices=[];
     %----------------------------------------------------------------------
-    %------------- Citações selecionadas apenas com base no fitness--------
+    %------------- CitaÃ§Ãµes selecionadas apenas com base no fitness--------
     %----------------------------------------------------------------------
-    f=rand(1)*a(end,5); %valor aleatório de fitness acumulado
+    f=rand(1)*a(end,5); %valor aleatÃ³rio de fitness acumulado
     for j=1:tipo2
-        f=rand(1)*a(end,5); %valor aleatório de fitness acumulado
-        ind=find(a(:,5)>=f,1); %índice do elemento de a que corresponde ao valor sorteado de fitness
-        while numel(find(indices==ind))~=0 %enquanto o índice sorteado já existir entre os sorteados
-            f=rand(1)*a(end,5); %valor aleatório de fitness acumulado
-            ind=find(a(:,5)>=f,1); %índice do elemento de a que corresponde ao valor sorteado de fitness
+        f=rand(1)*a(end,5); %valor aleatÃ³rio de fitness acumulado
+        ind=find(a(:,5)>=f,1); %Ã­ndice do elemento de a que corresponde ao valor sorteado de fitness
+        while numel(find(indices==ind))~=0 %enquanto o Ã­ndice sorteado jÃ¡ existir entre os sorteados
+            f=rand(1)*a(end,5); %valor aleatÃ³rio de fitness acumulado
+            ind=find(a(:,5)>=f,1); %Ã­ndice do elemento de a que corresponde ao valor sorteado de fitness
         end
         indices(j)=ind;
     end
     %-----------------------------------------------------------------------------------------------
-    %------------- Citações selecionadas com base no fitness ponderado pelo grau de entrada --------
+    %------------- CitaÃ§Ãµes selecionadas com base no fitness ponderado pelo grau de entrada --------
     %-----------------------------------------------------------------------------------------------
     for j=tipo2+1:tipo2+tipo1
-        f=rand(1)*a(end,9); %valor aleatório de fitness ponderado acumulado
-        ind=find(a(:,9)>=f,1); %índice do elemento de a que corresponde ao valor sorteado de fitness
-        while numel(find(indices==ind))~=0 %enquanto o índice sorteado já existir entre os sorteados
-            f=rand(1)*a(end,9); %valor aleatório de fitness acumulado
-            ind=find(a(:,9)>=f,1); %índice do elemento de a que corresponde ao valor sorteado de fitness
+        f=rand(1)*a(end,9); %valor aleatÃ³rio de fitness ponderado acumulado
+        ind=find(a(:,9)>=f,1); %Ã­ndice do elemento de a que corresponde ao valor sorteado de fitness
+        while numel(find(indices==ind))~=0 %enquanto o Ã­ndice sorteado jÃ¡ existir entre os sorteados
+            f=rand(1)*a(end,9); %valor aleatÃ³rio de fitness acumulado
+            ind=find(a(:,9)>=f,1); %Ã­ndice do elemento de a que corresponde ao valor sorteado de fitness
         end
         indices(j)=ind;
     end
     
     citados=[a(indices,2) a(indices,3) a(indices,1) a(indices,10)];
-    for j=1:c  %para todas as citações do artigo SOURCE
-        cit(j+seq,1)=an(art,2);       %número do artigo SOURCE
+    for j=1:c  %para todas as citaÃ§Ãµes do artigo SOURCE
+        cit(j+seq,1)=an(art,2);       %nÃºmero do artigo SOURCE
         cit(j+seq,2)=an(art,3);       %ano do artigo SOURCE
-        cit(j+seq,3)=an(art,1);       %periódico do artigo SOURCE
-        cit(j+seq,4)=citados(j,1);   %número do artigo TARGET
+        cit(j+seq,3)=an(art,1);       %periÃ³dico do artigo SOURCE
+        cit(j+seq,4)=citados(j,1);   %nÃºmero do artigo TARGET
         cit(j+seq,5)=citados(j,2);   %ano do artigo TARGET
-        cit(j+seq,6)=citados(j,3);   %periódico do artigo TARGET
-        cit(j+seq,7)=an(art,10);      %ediçao do periódico do artigo SOURCE
-        cit(j+seq,8)=citados(j,4);   %ediçao do periódico do artigo TARGET
+        cit(j+seq,6)=citados(j,3);   %periÃ³dico do artigo TARGET
+        cit(j+seq,7)=an(art,10);      %ediÃ§ao do periÃ³dico do artigo SOURCE
+        cit(j+seq,8)=citados(j,4);   %ediÃ§ao do periÃ³dico do artigo TARGET
     end
     seq=seq+c;
-%
-%
-%                    %serão feitas "tipo2" seleções apenas por fitness
-%                    f=rand(1,tipo2)*a(end,5); %vetor com "tipo2" elementos de valores sorteados de fitness acum
-%                                              %coluna 5 do arquivo a
-%                    %cria o vetor "citados" com os números dos artigos TARGET correspondentes
-%                    %     aos valores de ftness acumulado que foram gerados
-%                    citados=zeros(tipo2,3);          %vetor com os dados dos artigos TARGET citados
-%                    for j=1:tipo2
-%                        indice=find(a(:,5)>=f(j),1); %indice do elemento correspondente ao fitness sorteado (col 5)
-%                        citados(j,1)=a(indice,2);    %número do artigo TARGET citado
-%                        citados(j,2)=a(indice,3);    %ano do artigo TARGET citado
-%                        citados(j,3)=a(indice,1);    %periódico do artigo TARGET citado
-%                    end
-%                    %----------------------------------------------------------------------
-%                    %retirar os artigos selecionados apenas por fitness repetidos do vetor "citados"
-%                    semrep=citados;
-%                    unicos=unique(semrep(:,1)); %lista ordenada de valores sem repetição
-%                    n=histc(semrep(:,1),unicos);%lista com a contagem dos valores (histograma)
-%                    valrep=unicos(n>1);    %lista com os valores repetidos
-%                    qtdrep=n(find(n>1));   %lista com a quantidade de cada valor repetido
-%                    nrepet=numel(valrep);  %quantidade de valores repetidos
-%                    while nrepet~=0        %repetir enquanto houver valores repetidos
-%                        for i=1:nrepet
-%                            valor=valrep(i);                  %pega cada valor repetido
-%                            indrep=find(semrep==valrep(i));   %lista de índices de cada valor repetido
-%                            for t=2:qtdrep(i)                 %para todos as repetições exceto a primeira
-%                                novofitacum=rand(1)*a(end,5); %sorteia um novo valor de fitness acumulado
-%                                novoindice=find(a(:,5)>=novofitacum,1);
-%                                novovalor=a(novoindice,2);    %código do artigo correspondente ao fitness
-%                                novoano=a(novoindice,3);      %ano do artigo correspondente ao fitness
-%                                novoperiodico=a(novoindice,1);%periódico do artigo correspondente ao fitness
-%                                while novovalor==valor
-%                                    novofitacum=rand(1)*a(end,5); %sorteia um novo valor de fitness acumulado
-%                                    novoindice=find(a(:,5)>=novofitacum,1); %indice do elemento
-%                                    novovalor=a(novoindice,2);    %artigo correspondente ao fitness
-%                                    novoano=a(novoindice,3);      %ano do artigo correspondente ao fitness
-%                                    novoperiodico=a(novoindice,1);%periódico do artigo correspondente ao fitness
-%                                end
-%                                semrep(indrep(t),1)=novovalor;     %coloca o novo valor no lugar do repetido
-%                                semrep(indrep(t),2)=novoano;       %coloca o novo ano no lugar do repetido
-%                            end
-%                       end
-%                        %prepara o vetor resultante para nova inspeção de repetições
-%                        unicos=unique(semrep(:,1)); %lista ordenada de valores sem repetição
-%                        n=histc(semrep(:,1),unicos);%lista com a contagem dos valores (histograma)
-%                        valrep=unicos(n>1);         %lista com os valores repetidos
-%                        qtdrep=n(find(n>1));        %lista com a quantidade de cada valor repetido
-%                        nrepet=numel(valrep);       %quantidade de valores repetidos
-%                    end 
-%                    %------------salva as duplas SOURCE/TARGET no arquivo "cit"-------------------
-%                    citados=semrep; %vetor com os artigos TARGET citados pelo SOURCE ativo selecionadas apenas por fitness
-%                    for j=1:tipo2  %para todas as citações do artigo SOURCE
-%                        cit(j+seq,1)=an(art,2);       %número do artigo SOURCE
-%                        cit(j+seq,2)=an(art,3);       %ano do artigo SOURCE
-%                        cit(j+seq,3)=an(art,1);       %periódico do artigo SOURCE
-%                        cit(j+seq,4)=citados(j,1);   %número do artigo TARGET
-%                        cit(j+seq,5)=citados(j,2);   %ano do artigo TARGET
-%                        cit(j+seq,6)=citados(j,3);   %periódico do artigo TARGET
-%                    end
-%                    seq=seq+tipo2;
-%                    %----------------------------------------------------------------------
-%                    %------------- Citações selecionadas com base no fitness ponderado pelo grau de entrada --------
-%                    %----------------------------------------------------------------------
-%                    %serão feitas "tipo1" seleções por fitness ponderado por grau de entrada
-%                    f=rand(1,tipo1)*a(end,9); %vetor com "tipo1" elementos de valores sorteados de fitness ponderado acum
-%                                              %coluna 9 do arquivo a
-%                    %cria o vetor "citados" com os números dos artigos TARGET correspondentes
-%                    %     aos valores de fitness ponderado que foram gerados
-%                    for j=1:tipo1
-%                        indice=find(a(:,9)>=f(j),1); %indice do elemento correspondente ao fitness sorteado (col 9)
-%                        citados(j,1)=a(indice,2);    %número do artigo TARGET citado
-%                        citados(j,2)=a(indice,3);    %ano do artigo TARGET citado
-%                        citados(j,3)=a(indice,1);    %periódico do artigo TARGET citado
-%                    end
-%                    %----------------------------------------------------------------------
-%                    %retirar os artigos selecionados por fitness ponderado repetidos do vetor "citados"
-%                    semrep=citados;
-%                    unicos=unique(semrep(:,1)); %lista ordenada de valores sem repetição
-%                    n=histc(semrep(:,1),unicos);%lista com a contagem dos valores (histograma)
-%                    valrep=unicos(n>1);    %lista com os valores repetidos
-%                    qtdrep=n(find(n>1));   %lista com a quantidade de cada valor repetido
-%                    nrepet=numel(valrep);  %quantidade de valores repetidos
-%                    while nrepet~=0        %repetir enquanto houver valores repetidos
-%                        for i=1:nrepet
-%                            valor=valrep(i);                  %pega cada valor repetido
-%                            indrep=find(semrep==valrep(i));   %lista de índices de cada valor repetido
-%                            for t=2:qtdrep(i)                 %para todos as repetições exceto a primeira
-%                                novofitacum=rand(1)*a(end,9); %sorteia um novo valor de fitness ponderado
-%                                novoindice=find(a(:,9)>=novofitacum,1);
-%                                novovalor=a(novoindice,2);    %código do artigo correspondente ao fitness
-%                                novoano=a(novoindice,3);      %ano do artigo correspondente ao fitness
-%                                novoperiodico=a(novoindice,1);%periódico do artigo correspondente ao fitness
-%                                while novovalor==valor
-%                                    novofitacum=rand(1)*a(end,9); %sorteia um novo valor de fitness ponderado
-%                                    novoindice=find(a(:,9)>=novofitacum,1); %indice do elemento
-%                                    novovalor=a(novoindice,2);    %artigo correspondente ao fitness
-%                                    novoano=a(novoindice,3);      %ano do artigo correspondente ao fitness
-%                                    novoperiodico=a(novoindice,1);%periódico do artigo correspondente ao fitness
-%                                end
-%                                semrep(indrep(t),1)=novovalor;     %coloca o novo valor no lugar do repetido
-%                                semrep(indrep(t),2)=novoano;       %coloca o novo ano no lugar do repetido
-%                                semrep(indrep(t),3)=novoperiodico; %coloca o novo periódico no lugar do repetido
-%                            end
-%                        end
-%                        %prepara o vetor resultante para nova inspeção de repetições
-%                        unicos=unique(semrep(:,1)); %lista ordenada de valores sem repetição
-%                        n=histc(semrep(:,1),unicos);%lista com a contagem dos valores (histograma)
-%                        valrep=unicos(n>1);         %lista com os valores repetidos
-%                        qtdrep=n(find(n>1));        %lista com a quantidade de cada valor repetido
-%                        nrepet=numel(valrep);       %quantidade de valores repetidos
-%                    end
-%                    %------------salva as duplas SOURCE/TARGET no arquivo "cit"-------------------
-%                    citados=semrep; %vetor com os artigos TARGET citados pelo SOURCE ativo selecionadas por fitness ponderado
-%                    for j=1:tipo1  %para todas as citações do artigo SOURCE
-%                        cit(j+seq,1)=an(art,2);       %número do artigo SOURCE
-%                        cit(j+seq,2)=an(art,3);       %ano do artigo SOURCE
-%                        cit(j+seq,3)=an(art,1);       %periódico do artigo SOURCE
-%                        cit(j+seq,4)=citados(j,1);   %número do artigo TARGET
-%                        cit(j+seq,5)=citados(j,2);   %ano do artigo TARGET
-%                        cit(j+seq,6)=citados(j,3);   %periódico do artigo TARGET
-%                    end
-%                    seq=seq+tipo1;
 end
